bca81b5f89705ddb7d94480935e98b093f53fbef
GalaxyXie <771472141@qq.com>
2019-08-19 21:44:23 +0800

    [Laura][finied reactor of GiledRose]

--- src/main/java/gildedRose/GildedRose.java
+++ src/main/java/gildedRose/GildedRose.java
@@ -7,7 +7,12 @@ public class GildedRose {
     }
 
     public void updateQuality() {
+        Roses roses=new Roses();
         for (int i = 0; i < items.length; i++) {
+          Item temp=roses.getItem(items[i]);
+          temp.updateQualityAndSellin();
+          items[i].setQuality(temp.quality);
+          items[i].setSellIn(temp.sellIn);
         }
     }
 }
